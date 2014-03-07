5bc3573e1a2fe9e62c307c25a597d9966e00525a
Andrew Kane <acekane1@gmail.com>
2014-03-06 21:49:09 -0800

    Added time zone tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -94,6 +94,14 @@ module TestGroupdate
     assert_result_time :day, "2013-05-03 02:00:00 UTC", "2013-05-03 02:00:00", false, :day_start => 2
   end
 
+  def test_test_day_end_of_day_with_time_zone_day_start_2am
+    assert_result_time :day, "2013-05-03 02:00:00 PDT", "2013-05-04 07:59:59", true, :day_start => 2
+  end
+
+  def test_test_day_start_of_day_with_time_zone_day_start_2am
+    assert_result_time :day, "2013-05-03 02:00:00 PDT", "2013-05-03 09:00:00", true, :day_start => 2
+  end
+
   # week
 
   def test_week_end_of_week
@@ -158,6 +166,14 @@ module TestGroupdate
     assert_result_time :week, "2013-03-17 02:00:00 UTC", "2013-03-17 02:00:00", false, :day_start => 2
   end
 
+  def test_week_end_of_week_day_with_time_zone_start_2am
+    assert_result_time :week, "2013-03-17 02:00:00 PDT", "2013-03-24 08:59:59", true, :day_start => 2
+  end
+
+  def test_week_start_of_week_day_with_time_zone_start_2am
+    assert_result_time :week, "2013-03-17 02:00:00 PDT", "2013-03-17 09:00:00", true, :day_start => 2
+  end
+
   # month
 
   def test_month_end_of_month
@@ -184,6 +200,14 @@ module TestGroupdate
 
   def test_month_start_of_month_day_start_2am
     assert_result_time :month, "2013-03-01 02:00:00 UTC", "2013-03-01 02:00:00", false, :day_start => 2
+  end
+
+  def test_month_end_of_month_with_time_zone_day_start_2am
+    assert_result_time :month, "2013-03-01 02:00:00 PST", "2013-04-01 08:59:59", true, :day_start => 2
+  end
+
+  def test_month_start_of_month_with_time_zone_day_start_2am
+    assert_result_time :month, "2013-03-01 02:00:00 PST", "2013-03-01 10:00:00", true, :day_start => 2
   end
 
   # year
@@ -214,6 +238,14 @@ module TestGroupdate
     assert_result_time :year, "2013-01-01 02:00:00 UTC", "2013-01-01 02:00:00", false, :day_start => 2
   end
 
+  def test_year_end_of_year_with_time_zone_day_start_2am
+    assert_result_time :year, "2013-01-01 02:00:00 PST", "2014-01-01 09:59:59", true, :day_start => 2
+  end
+
+  def test_year_start_of_year_with_time_zone_day_start_2am
+    assert_result_time :year, "2013-01-01 02:00:00 PST", "2013-01-01 10:00:00", true, :day_start => 2
+  end
+
   # hour of day
 
   def test_hour_of_day_end_of_hour
@@ -242,6 +274,14 @@ module TestGroupdate
     assert_result :hour_of_day, 0, "2013-01-01 02:00:00", false, :day_start => 2
   end
 
+  def test_hour_of_day_end_of_day_with_time_zone_day_start_2am
+    assert_result :hour_of_day, 23, "2013-01-01 09:59:59", true, :day_start => 2
+  end
+
+  def test_hour_of_day_start_of_day_with_time_zone_day_start_2am
+    assert_result :hour_of_day, 0, "2013-01-01 10:00:00", true, :day_start => 2
+  end
+
   # day of week
 
   def test_day_of_week_end_of_day
@@ -268,6 +308,14 @@ module TestGroupdate
 
   def test_day_of_week_start_of_day_day_start_2am
     assert_result :day_of_week, 3, "2013-01-02 02:00:00", false, :day_start => 2
+  end
+
+  def test_day_of_week_end_of_day_with_time_zone_day_start_2am
+    assert_result :day_of_week, 3, "2013-01-03 09:59:59", true, :day_start => 2
+  end
+
+  def test_day_of_week_start_of_day_with_time_zone_day_start_2am
+    assert_result :day_of_week, 3, "2013-01-02 10:00:00", true, :day_start => 2
   end
 
   # zeros
