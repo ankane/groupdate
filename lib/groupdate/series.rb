f692eaabec019543b8bd4131da81f1957a267e33
Tres Trantham <tres@trestrantham.com>
2014-06-13 23:45:32 -0400

    Allow a model to define its own calculation method

--- lib/groupdate/series.rb
+++ lib/groupdate/series.rb
@@ -170,7 +170,7 @@ module Groupdate
     # clone to prevent modifying original variables
     def method_missing(method, *args, &block)
       # https://github.com/rails/rails/blob/master/activerecord/lib/active_record/relation/calculations.rb
-      if ActiveRecord::Calculations.method_defined?(method)
+      if ActiveRecord::Calculations.method_defined?(method) || method == :to_groupdate
         clone.perform(method, *args, &block)
       elsif @relation.respond_to?(method)
         series = clone
