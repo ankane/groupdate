dc8fc08d470ebecbfe2a1da5557ce0c888c63f91
Andrew Kane <acekane1@gmail.com>
2014-06-22 04:32:37 -0700

    Added enumerable only test

--- test/enumerable_test.rb
+++ test/enumerable_test.rb
@@ -4,6 +4,17 @@ unless ActiveRecord::VERSION::MAJOR == 3 and ActiveRecord::VERSION::MINOR < 2
   class TestEnumerable < Minitest::Unit::TestCase
     include TestGroupdate
 
+    def test_enumerable
+      user_a = User.new(created_at: utc.parse("2014-01-21"))
+      user_b = User.new(created_at: utc.parse("2014-03-14"))
+      expected = {
+        utc.parse("2014-01-01") => [user_a],
+        utc.parse("2014-02-01") => [],
+        utc.parse("2014-03-01") => [user_b]
+      }
+      assert_equal expected, [user_a, user_b].group_by_month(&:created_at)
+    end
+
     def call_method(method, field, options)
       Hash[ User.all.to_a.send(:"group_by_#{method}", options){|u| u.send(field) }.map{|k, v| [k, v.size] } ]
     end
