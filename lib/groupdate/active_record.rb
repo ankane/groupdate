6e051d949a92d5245414d20a96fbcf918570c7ba
Andrew Kane <acekane1@gmail.com>
2016-01-31 23:05:40 -0800

    No need for Rails 3 hack

--- lib/groupdate/active_record.rb
+++ lib/groupdate/active_record.rb
@@ -6,23 +6,6 @@ require "groupdate/series"
 ActiveRecord::Base.send(:extend, Groupdate::Scopes)
 
 module ActiveRecord
-  class Relation
-    if ActiveRecord::VERSION::MAJOR == 3 && ActiveRecord::VERSION::MINOR < 2
-
-      def method_missing_with_hack(method, *args, &block)
-        if Groupdate::METHODS.include?(method)
-          scoping { @klass.send(method, *args, &block) }
-        else
-          method_missing_without_hack(method, *args, &block)
-        end
-      end
-      alias_method_chain :method_missing, :hack
-
-    end
-  end
-end
-
-module ActiveRecord
   module Associations
     class CollectionProxy
       if ActiveRecord::VERSION::MAJOR == 3
