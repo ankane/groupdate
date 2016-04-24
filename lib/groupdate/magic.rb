0ec3b9295c5f6ed69e8949d74cb1a423ff17ef56
Andrew Kane <acekane1@gmail.com>
2016-04-23 17:19:39 -0700

    No need for BigDecimal check

--- lib/groupdate/magic.rb
+++ lib/groupdate/magic.rb
@@ -130,7 +130,7 @@ module Groupdate
           lambda { |k| k.to_i }
         else
           utc = ActiveSupport::TimeZone["UTC"]
-          lambda { |k| (k.is_a?(String) ? utc.parse(k) : (k.is_a?(BigDecimal) ? Time.at(k) : k.to_time)).in_time_zone(time_zone) }
+          lambda { |k| (k.is_a?(String) ? utc.parse(k) : k.to_time).in_time_zone(time_zone) }
         end
 
       count =
