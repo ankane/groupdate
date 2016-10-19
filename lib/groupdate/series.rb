bc891b1bec7bbbef7efc030927a3e3d3d6d84b32
Charles Maresh <cmaresh@gmail.com>
2016-10-19 12:13:53 -0500

    Clean up methods to remove usage of `try`

--- lib/groupdate/series.rb
+++ lib/groupdate/series.rb
@@ -10,7 +10,7 @@ module Groupdate
     # clone to prevent modifying original variables
     def method_missing(method, *args, &block)
       # https://github.com/rails/rails/blob/master/activerecord/lib/active_record/relation/calculations.rb
-      if ActiveRecord::Calculations.method_defined?(method) || custom_calculation_methods.include?(method)
+      if ActiveRecord::Calculations.method_defined?(method) || custom_calculation_method?(method)
         magic.perform(relation, method, *args, &block)
       elsif relation.respond_to?(method, true)
         Groupdate::Series.new(magic, relation.send(method, *args, &block))
@@ -29,14 +29,18 @@ module Groupdate
 
     private
 
-    def model
-      return if !relation.respond_to?(:klass)
-
-      relation.klass
+    def custom_calculation_method?(method)
+      custom_calculation_methods.include?(method)
     end
 
     def custom_calculation_methods
-      model.try(:groupdate_calculation_methods) || []
+      return [] if !model.respond_to?(:groupdate_calculation_methods)
+      model.groupdate_calculation_methods
+    end
+
+    def model
+      return if !relation.respond_to?(:klass)
+      relation.klass
     end
   end
 end
