12183fb3400fcc440aeaa831d741668d7a89ae4a
Andrew <acekane1@gmail.com>
2018-02-21 18:09:50 -0800

    Extend is public

--- lib/groupdate/active_record.rb
+++ lib/groupdate/active_record.rb
@@ -2,5 +2,5 @@ require "active_record"
 require "groupdate/scopes"
 require "groupdate/relation"
 
-ActiveRecord::Base.send(:extend, Groupdate::Scopes)
+ActiveRecord::Base.extend(Groupdate::Scopes)
 ActiveRecord::Relation.include(Groupdate::Relation)
