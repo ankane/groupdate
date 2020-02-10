ea328acf39cea05566524f4ce3911e76f03d7644
Andrew Kane <andrew@chartkick.com>
2020-02-09 23:56:44 -0800

    Improved assert_raises

--- test/permit_test.rb
+++ test/permit_test.rb
@@ -2,12 +2,16 @@ require_relative "test_helper"
 
 class PermitTest < Minitest::Test
   def test_permit
-    error = assert_raises(ArgumentError) { User.group_by_period(:day, :created_at, permit: %w(week)).count }
+    error = assert_raises(ArgumentError) do
+      User.group_by_period(:day, :created_at, permit: %w(week)).count
+    end
     assert_equal "Unpermitted period", error.message
   end
 
   def test_permit_bad_period
-    error = assert_raises(ArgumentError) { User.group_by_period(:bad_period, :created_at).count }
+    error = assert_raises(ArgumentError) do
+      User.group_by_period(:bad_period, :created_at).count
+    end
     assert_equal "Unpermitted period", error.message
   end
 
