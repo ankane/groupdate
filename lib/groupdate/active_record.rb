d4d88e5bcc65fadace19462a393e31362a397eac
Benjamin Borowski <ben@weareokidoki.com>
2021-03-22 21:10:15 -0700

    Use lazy load hook to avoid config issues

--- lib/groupdate/active_record.rb
+++ lib/groupdate/active_record.rb
@@ -2,5 +2,7 @@ require "active_record"
 require "groupdate/query_methods"
 require "groupdate/relation"
 
-ActiveRecord::Base.extend(Groupdate::QueryMethods)
-ActiveRecord::Relation.include(Groupdate::Relation)
+ActiveSupport.on_load(:active_record) do
+  ActiveRecord::Base.extend(Groupdate::QueryMethods)
+  ActiveRecord::Relation.include(Groupdate::Relation)
+end
