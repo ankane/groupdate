d739bb62d067b75280dc5a9a87247d4b6a48d609
Andrew Kane <andrew@ankane.org>
2021-07-29 11:30:09 -0700

    Revert "Use ActiveRecord::Schema.define for test setup [skip ci]"
    This reverts commit 644ae471df176e5ba019ad79a1f4e7a1ebbd63be.

--- test/support/activerecord.rb
+++ test/support/activerecord.rb
@@ -24,26 +24,28 @@ end
 
 # migrations
 if ENV["ADAPTER"] == "redshift"
-  ActiveRecord::Schema.define do
-    drop_table(:users, force: :cascade) if table_exists?(:users)
-    drop_table(:posts, force: :cascade) if table_exists?(:posts)
-
-    execute "CREATE TABLE users (id INT IDENTITY(1,1) PRIMARY KEY, name VARCHAR(255), score INT, created_at DATETIME, created_on DATE);"
-    execute "CREATE TABLE posts (id INT IDENTITY(1,1) PRIMARY KEY, user_id INT REFERENCES users, created_at DATETIME);"
+  if ActiveRecord::Migration.table_exists?(:users)
+    ActiveRecord::Migration.drop_table(:users, force: :cascade)
   end
-else
-  ActiveRecord::Schema.define do
-    create_table :users, force: true do |t|
-      t.string :name
-      t.integer :score
-      t.datetime :created_at
-      t.column :deleted_at, :timestamptz if ENV["ADAPTER"] == "postgresql"
-      t.date :created_on
-    end
 
-    create_table :posts, force: true do |t|
-      t.references :user
-      t.datetime :created_at
-    end
+  if ActiveRecord::Migration.table_exists?(:posts)
+    ActiveRecord::Migration.drop_table(:posts, force: :cascade)
+  end
+
+  ActiveRecord::Migration.execute "CREATE TABLE users (id INT IDENTITY(1,1) PRIMARY KEY, name VARCHAR(255), score INT, created_at DATETIME, created_on DATE);"
+
+  ActiveRecord::Migration.execute "CREATE TABLE posts (id INT IDENTITY(1,1) PRIMARY KEY, user_id INT REFERENCES users, created_at DATETIME);"
+else
+  ActiveRecord::Migration.create_table :users, force: true do |t|
+    t.string :name
+    t.integer :score
+    t.datetime :created_at
+    t.column :deleted_at, :timestamptz if ENV["ADAPTER"] == "postgresql"
+    t.date :created_on
+  end
+
+  ActiveRecord::Migration.create_table :posts, force: true do |t|
+    t.references :user
+    t.datetime :created_at
   end
 end
